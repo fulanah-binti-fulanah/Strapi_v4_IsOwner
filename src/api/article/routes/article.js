2b9f21472c0f9d5ecf5e811f931b103a51f475db
nextrapi <saucetinc@gmail.com>
2022-01-28 06:44:01 -0500

    Configure routes to include policies and middlewares

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -5,5 +5,41 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::article.article');
+// Modify these to match your Content-type
+const uid = 'api::article.article';
+const field = 'author';
+// Only modify these if the middleware or policy name is different
+const SetOwner = {
+    name: 'global::SetOwner',
+    config: {
+        field,
+        uid,
+    },
+};
+const IsOwner = {
+    name: 'global::IsOwner',
+    config: {
+        field,
+        uid,
+    },
+};
+module.exports = createCoreRouter(uid, {
+    config: {
+        create: {
+            middlewares: [SetOwner],
+        },
+        find: {
+            policies: [IsOwner],
+        },
+        findOne: {
+            policies: [IsOwner],
+        },
+        delete: {
+            policies: [IsOwner],
+        },
+        update: {
+            policies: [IsOwner],
+            middlewares: [SetOwner],
+        },
+    },
+});
